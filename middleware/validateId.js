0d5c0ff1c45de037220e3892ad0b25a297c1ae31
Alessandro <deje0104@algonquinlive.com>
2022-04-09 13:50:52 -0400

    Created middleware to validate note id

---
+++ middleware/validateId.js
@@ -0,0 +1,23 @@
+const {data} = require('../data/data.js')
+
+function validateId (req, res, next){
+    const id = parseInt(req.params.id)
+    const index = data.findIndex(note => note.id === id)
+    if (index < 0) {
+      res.status(404).send({
+        errors: [
+          {
+            status: '404',
+            title: 'Resource does not exist',
+            description: `We could not find a note with id: ${id}`
+          }
+        ]
+      })
+    } else {
+        req.noteIndex = index
+      next();
+    }
+
+}
+
+module.exports = validateId
