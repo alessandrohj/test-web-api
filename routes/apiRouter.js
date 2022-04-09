c43db782d46d48fac0f9df9a3f43bb713ab4a8b2
Alessandro <deje0104@algonquinlive.com>
2022-04-09 13:53:34 -0400

    implemented validateNoteId

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,5 +1,12 @@
 const express = require('express')
-const data = require('../data/data.json');
+const data = require('../data/data.js');
+const validateNoteId = require('../middleware/validateId')
 const router = express.Router()
 
+router.use('/noteId', validateNoteId)
+
 router.get('/', (req, res) => res.send({data: data}))
+
+
+
+module.exports = router
